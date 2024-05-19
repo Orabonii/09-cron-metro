3d45550ef1e66012b901fd4c20db8c423e0fa06c
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 18:10:28 -0300

    Add files via upload

---
+++ projeto09/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import Cronometro from './Components/Projeto09/Projeto09'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <Cronometro/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
